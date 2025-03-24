15e686493692a7e3b8db0f10af8f011d18127db4
CMAG-98 <cesarmateoaguilargarcia1@gmail.com>
2025-03-23 23:15:11 -0500

    finalización HTML y llamado a API de countrys en personalización

---
+++ assets/Controllers/personalizacion.mjs
@@ -0,0 +1,30 @@
+import Helpers from './helpers.mjs'
+
+class Personalizacion {
+
+    static async cargarPaises() { 
+        
+        try {            
+            let response = await fetch("http://127.0.0.1:5000/countries");
+            
+            if (!response.ok) {
+                throw new Error(`Error HTTP: ${response.status}`);
+            }
+
+            let data = await response.json();
+            console.log(data);
+
+            Helpers.fullSelectors("#paises", data, "Click para Expandir");
+            
+        } catch (error) {
+            console.error("Error cargando países:", error);
+            document.querySelector('#paises').innerHTML = `<option>Error al cargar países</option>`;
+        }
+    }
+}
+
+Personalizacion.cargarPaises();
+
+
+
+
