04d65ad68244cc3f9ad7d0318acbc0b95be0350f
CMAG-98 <cesarmateoaguilargarcia1@gmail.com>
2025-04-05 15:19:58 -0500

    CSS formulario de registro, y estandarización de llamado de API de banderas

--- assets/Controllers/registro.mjs
+++ assets/Controllers/registro.mjs
@@ -1,41 +1,7 @@
 import User from '../../funciones/user.mjs';
 import  Helpers  from './helpers.mjs'
 
-class Regist{
-    static async loadCountries() { 
-        try {            
-            let response = await fetch("http://127.0.0.1:5000/countries");
-
-            if (!response.ok) {
-                throw new Error(`Error HTTP: ${response.status}`);
-            }
-
-            let data = await response.json();
-            console.log(data);
-
-            Helpers.fullSelectors("#countries", data, "Click para Expandir");
-            let countrySelect = document.querySelector("#countries");
-            let flagImg = document.querySelector("#flag");
-
-            // Agregar evento para actualizar la bandera en tiempo real
-            countrySelect.addEventListener("change", () => {
-            let country = countrySelect.value;
-
-            if (country) {
-                flagImg.src = `https://flagsapi.com/${country}/shiny/64.png`;
-                flagImg.style.display = "block"; // Asegurar que la imagen sea visible
-            } else {
-                flagImg.src = "";
-                flagImg.style.display = "none"; // Ocultar si no hay selección
-            }
-            });
-            
-        } catch (error) {
-            console.error("Error cargando países:", error);
-            document.querySelector('#countries').innerHTML = `<option>Error al cargar países</option>`;
-        }
-    }
-}
+Helpers.loadCountries();
 
 document.querySelector("#register").addEventListener("click", async (e) => {
     
