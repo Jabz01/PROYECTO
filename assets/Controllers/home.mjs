db1fec7781cbfc55b0bdc5a6cc22321dc534802f
Jeronimo Bastidas <jerobaza1@gmail.com>
2025-04-06 22:02:46 -0500

    Creación del home

---
+++ assets/Controllers/home.mjs
@@ -0,0 +1,9 @@
+import { Board } from "../../funciones/board.mjs";
+import { createBoard } from "../../funciones/bot.mjs";
+
+document.addEventListener('DOMContentLoaded', () => {
+    let exampleMap = new Board();
+    let MapWithPieces = createBoard(exampleMap, { x: 10, y: 10 });
+
+    MapWithPieces.renderOn("battleMapContainer", 10, null, true);
+});
