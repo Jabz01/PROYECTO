e0c2e0d5961b7889ab15a3eda73c9e1c9b665f31
Jeronimo Bastidas <jerobaza1@gmail.com>
2025-04-07 21:16:51 -0500

    Boton de exportar estilizado, más limpieza del localStorage al salir

--- assets/Controllers/home.mjs
+++ assets/Controllers/home.mjs
@@ -4,6 +4,13 @@ import { createBoard } from "../../funciones/bot.mjs";
 document.addEventListener('DOMContentLoaded', () => {
     let exampleMap = new Board();
     let MapWithPieces = createBoard(exampleMap, { x: 10, y: 10 });
-
+    const exitButton = document.getElementById("exitButton");
     MapWithPieces.renderOn("battleMapContainer", 10, null, true);
+
+    exitButton.addEventListener("click", () => {
+        localStorage.clear();
+        alert("LocalStorage ha sido borrado.");
+
+    });
+    
 });
